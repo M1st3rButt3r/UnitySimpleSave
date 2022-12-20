af88bf85c56e6955278d1bac1a2e3b07a1efa0b2
Bennet Rupnow <bennet.rupnow@gym-meiendorf.de>
2022-12-20 20:30:06 +0100

    ✔Add generic save model

---
+++ Runtime/SaveModels/SimpleSaveModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+[Serializable]
+public abstract class SimpleSaveModel : ICloneable
+{
+    public object Clone()
+    {
+        return MemberwiseClone();
+    }
+
+    public abstract void Set(object obj);
+
+    public abstract T Get<T>();
+}
